27ba06d34b0ef3fd2221a5ba9061f725962b3f57
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-07-20 17:29:49 +0200

    finished day

--- src/Controller/TravelController.php
+++ src/Controller/TravelController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Travel;
 use App\Repository\TravelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
