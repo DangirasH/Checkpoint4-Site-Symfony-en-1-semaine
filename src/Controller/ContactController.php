bd9a74f2f036656a0797d27b2ef765bf75a1304f
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-07-21 13:47:24 +0200

    finished contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/contact', name: 'contact_')]
+class ContactController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET', 'POST'])]
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+            $email = (new Email())
+                ->from($contactFormData->getEmail())
+                ->to('test@test.com')
+                ->subject('Message Split Screen')
+                ->html($this->renderView('contact/_email.html.twig', ['form' => $contactFormData]));
+            $mailer->send($email);
+            $this->addFlash('success', 'Votre message a bien été envoyé');
+            return $this->redirectToRoute('contact_index');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ContactForm;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => '<i class="bi bi-person my-2"></i> Prénom',
+                'label_html' => true,
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => '<i class="bi bi-person"></i> Nom',
+                'label_html' => true,
+            ])
+            ->add('phone', TelType::class, [
+                'label' => '<i class="bi bi-telephone my-5"></i> Téléphone',
+                'label_html' => true,
+            ])
+            ->add('email', EmailType::class, [
+                'label' => '<i class="bi bi-envelope"></i> Email',
+                'label_html' => true,
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => '<i class="bi bi-pen"></i> Message',
+                'label_html' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactForm::class,
+        ]);
+    }
+}
